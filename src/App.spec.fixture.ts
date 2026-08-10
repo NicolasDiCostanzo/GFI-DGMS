7d8af285a0193cb54ce1d7b9cedf860727e0066c
Nicolas Di Costanzo <ndicostanzo>
2026-08-10 12:10:00 +0200

    test: move App spec fixtures to local fixture file

---
+++ src/App.spec.fixture.ts
@@ -0,0 +1,45 @@
+import { Currency } from '@/shared/types/Currency';
+import { MapColors } from '@/sovereign/domain/constants/MapColors';
+import { Country, CountryId } from '@/sovereign/domain/Country';
+import { SimulationResults } from '@/sovereign/domain/SimulationResults';
+import { TargetBudget } from '@/sovereign/domain/TargetBudget';
+
+export const GERMANY = new Country(
+    CountryId('276'),
+    'Germany',
+    500,
+    new TargetBudget(1000, Currency.USD()),
+    10,
+    5,
+    10000,
+    5000,
+);
+
+export const FRANCE = new Country(
+    CountryId('250'),
+    'France',
+    300,
+    new TargetBudget(800, Currency.EUR()),
+    8,
+    4,
+    8000,
+    4000,
+);
+
+export const RESULTS: SimulationResults = {
+    fundingProgress: 0.75,
+    additionalJobs: 2500,
+    additionalCO2Tonnes: 1250,
+    isOverTarget: false,
+    colorHex: MapColors.ORANGE,
+};
+
+export function deferred<T>() {
+    let resolve!: (value: T) => void;
+    let reject!: (reason: Error) => void;
+    const promise = new Promise<T>((res, rej) => {
+        resolve = res;
+        reject = rej;
+    });
+    return { promise, resolve, reject };
+}
