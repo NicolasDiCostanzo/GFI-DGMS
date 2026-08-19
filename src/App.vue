10817ecc4461242f8b83156dd9e212efd3743b35
Nicolas Di Costanzo <ndicostanzo>
2026-08-19 09:56:40 +0200

    feat(App.vue): Add font-family

--- src/App.vue
+++ src/App.vue
@@ -222,6 +222,16 @@ function handleSidebarClosing(): void {
     width: 100%;
     height: 100%;
     overflow: hidden;
+    font-family: var(
+        --font-family,
+        system-ui,
+        -apple-system,
+        'Segoe UI',
+        Roboto,
+        'Helvetica Neue',
+        Arial,
+        sans-serif
+    );
 }
 
 .sr-only {
